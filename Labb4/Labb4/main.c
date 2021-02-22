7a89c7468a374f8b629d1f8af4e6b4a81964cf54
LeifTeorin <hjalle2000@gmail.com>
2021-02-22 10:36:49 +0100

    here we go again

---
+++ Labb4/Labb4/main.c
@@ -0,0 +1,18 @@
+/*
+ * Labb4.c
+ *
+ * Created: 2021-02-22 10:36:20
+ * Author : hjall
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
